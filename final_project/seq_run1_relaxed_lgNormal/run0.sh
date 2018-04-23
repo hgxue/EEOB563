c1dbc72b1801d1f238dab0ad4aa22b2448fb1bf8
Hylia Gao <hyliagao0829@gmail.com>
2018-04-22 23:58:27 -0500

    update

---
+++ final_project/seq_run1_relaxed_lgNormal/run0.sh
@@ -0,0 +1,7 @@
+cd "/Users/apple/Desktop/EEOB563/final_project/seq_run1_relaxed_lgNormal/step0"
+java -cp "/Applications/BEAST 2.3.2/AppStore.app/Contents/Java/Classes:/Applications/BEAST 2.3.2/AppStore.app/Contents/Java/launcher.jar:/Applications/BEAST 2.3.2/lib/beast.jar:/Applications/BEAST 2.3.2/lib/beast.src.jar:/Users/apple/Library/Application Support/BEAST/2.3/MODEL_SELECTION/lib/MODEL_SELECTION.addon.jar:/Users/apple/Library/Application Support/BEAST/2.3/SA/lib/SA.addon.jar:/Applications/BEAST 2.3.2/lib/beast.src.jar" beast.app.beastapp.BeastMain -overwrite -java -seed 780207619 beast.xml
+
+cp /Users/apple/Desktop/EEOB563/final_project/seq_run1_relaxed_lgNormal/step0/beast.xml.state /Users/apple/Desktop/EEOB563/final_project/seq_run1_relaxed_lgNormal/step1
+cd "/Users/apple/Desktop/EEOB563/final_project/seq_run1_relaxed_lgNormal/step1"
+java -cp "/Applications/BEAST 2.3.2/AppStore.app/Contents/Java/Classes:/Applications/BEAST 2.3.2/AppStore.app/Contents/Java/launcher.jar:/Applications/BEAST 2.3.2/lib/beast.jar:/Applications/BEAST 2.3.2/lib/beast.src.jar:/Users/apple/Library/Application Support/BEAST/2.3/MODEL_SELECTION/lib/MODEL_SELECTION.addon.jar:/Users/apple/Library/Application Support/BEAST/2.3/SA/lib/SA.addon.jar:/Applications/BEAST 2.3.2/lib/beast.src.jar" beast.app.beastapp.BeastMain -resume -java -seed 1860434221 beast.xml
+
---
+++ final_project/seq_run1_relaxed_lgNormal/step0/resume.sh
@@ -0,0 +1,3 @@
+cd "/Users/apple/Desktop/EEOB563/final_project/seq_run1_relaxed_lgNormal/step0"
+java -cp "/Applications/BEAST 2.3.2/AppStore.app/Contents/Java/Classes:/Applications/BEAST 2.3.2/AppStore.app/Contents/Java/launcher.jar:/Applications/BEAST 2.3.2/lib/beast.jar:/Applications/BEAST 2.3.2/lib/beast.src.jar:/Users/apple/Library/Application Support/BEAST/2.3/MODEL_SELECTION/lib/MODEL_SELECTION.addon.jar:/Users/apple/Library/Application Support/BEAST/2.3/SA/lib/SA.addon.jar:/Applications/BEAST 2.3.2/lib/beast.src.jar" beast.app.beastapp.BeastMain -resume -java -seed 780207619 beast.xml
+
